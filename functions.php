a566814409dc454dcb4ac3391e5a67009c41f912
Paul Hart <atomicjeep@gmail.com>
2019-09-16 14:03:07 +0100

    Write out docs

--- functions.php
+++ functions.php
@@ -11,27 +11,63 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_TEC_VERSION', '1.0.0' );
+define('CHILD_THEME_TEC_VERSION', '1.0.0');
 
 /**
  * Enqueue styles & javascript
  */
-function child_enqueue_shiz() {
-	wp_enqueue_style( 'tec-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_TEC_VERSION, 'all' );
-	wp_enqueue_script('tec-theme-js', get_stylesheet_directory_uri() . '/tec-focus.js',  array() , CHILD_THEME_TEC_VERSION, 'all' );
+function child_enqueue_shiz()
+{
+    wp_enqueue_style('tec-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_TEC_VERSION, 'all');
+    wp_enqueue_script('tec-theme-js', get_stylesheet_directory_uri() . '/tec-focus.js', array(), CHILD_THEME_TEC_VERSION, 'all');
 }
-add_action( 'wp_enqueue_scripts', 'child_enqueue_shiz', 15 );
+add_action('wp_enqueue_scripts', 'child_enqueue_shiz', 15);
 
 
 //Add Astra Hooks - Docs
-add_action( 'astra_primary_content_bottom', 'add_tec_docs' );
-function add_tec_docs() {
-    if ( is_singular( array( 'events', 'page', 'post' ) ) and !is_front_page() ) {
+add_action('astra_primary_content_bottom', 'add_tec_docs');
+function add_tec_docs()
+{
+    if (is_singular(array( 'events', 'page', 'post' )) and !is_front_page()) {
+        // START
+        if (have_rows('files')) :
+            echo '<aside class="page type-page status-publish ast-article-single tec-docs" style="margin-top:20px;">';
+            echo '<h3><i class="fas fa-file-alt"></i>Documents</h3>';
+            echo '<ul class="tec-docs" style="list-style:none;margin-left:0;">';
+            while (have_rows('files')) :
+                the_row();
+                $localdoc = get_sub_field('document');
+                echo '<li class="tec-doc">';
+                if ($localdoc) :
+                    echo '<img style="height:20px;" src="'  .echo $localdoc['icon'];
+                    .   " alt=".echo $localdoc['title'];
+                    .   '"><a href="'   .echo $localdoc['url'];.        '"></img>'
+                endif
+                echo $localdoc['title'];
+                echo '('.$localdoc['subtype'].')';
+                if ($localdoc) :
+                    echo '</a>';
+                endif;
+                echo '</li>';
+            endwhile;
+            echo '</ul>';
+            echo '</aside>';
+        endif;
+        if (!have_rows('files')) :
+        endif;
+// END
 
-    	// Output Twitter Share button - via plugin
-    	echo "<br>";
-    	echo do_shortcode('[scriptless]');
-	}
+
+
+
+
+
+
+
+        // Output Twitter Share button - via plugin
+        echo "<br>";
+        echo do_shortcode('[scriptless]');
+    }
 }
 
 
@@ -45,39 +81,38 @@ function add_tec_docs() {
 
 
 // Shortcode to render edit button where we want it
-add_shortcode( 'tec_edit_button', function () {
-	$out = edit_post_link('Edit This','','','','tec-edit-button');
-	
-	return $out;
-} );
+add_shortcode('tec_edit_button', function () {
+    $out = edit_post_link('Edit This', '', '', '', 'tec-edit-button');
+    
+    return $out;
+});
 
 
 // Shortcode to calculate a date range
-add_shortcode( 'tec_event_range', function () {
-	if( get_field('when_end') ):
-					$startdate = new DateTime(get_field('when', false, false));
-					$endate = new DateTime(get_field('when_end', false, false));
-					
-					if ( $startdate->format('F') !=  $endate->format('F')):
-						$out = $startdate->format('j M Y') . ' - ' . $endate->format('j M Y');
-					endif;
-					
-					if ( $startdate->format('F') ==  $endate->format('F')):
-						$out = $startdate->format('j') . ' - ' . $endate->format('j F Y');
-					endif;	
-	
-					 	
-	elseif (!get_field('when_end')):
-					$out = get_field('when');
-	endif;
-					return $out;
+add_shortcode('tec_event_range', function () {
+    if (get_field('when_end')) :
+                    $startdate = new DateTime(get_field('when', false, false));
+                    $endate = new DateTime(get_field('when_end', false, false));
+                    
+        if ($startdate->format('F') !=  $endate->format('F')) :
+            $out = $startdate->format('j M Y') . ' - ' . $endate->format('j M Y');
+        endif;
+                    
+        if ($startdate->format('F') ==  $endate->format('F')) :
+            $out = $startdate->format('j') . ' - ' . $endate->format('j F Y');
+        endif;
+    elseif (!get_field('when_end')) :
+                    $out = get_field('when');
+        endif;
+                    return $out;
 });
 
 
 //Set default image for event content types
 add_filter('acf/prepare_field/name=event-image', 'acf_use_default_image');
 
-function acf_use_default_image($field) {
+function acf_use_default_image($field)
+{
     if ($field['value'] === null) {
         $field['value'] = 354; // attachment id
     }
@@ -86,19 +121,21 @@ function acf_use_default_image($field) {
 
 
 // Add "nocookie" To WordPress oEmbeded Youtube Videos
-function ev_youtube_nocookie_oembed( $html ) {
-	return str_replace( 'youtube.com', 'youtube-nocookie.com', $html );
+function ev_youtube_nocookie_oembed($html)
+{
+    return str_replace('youtube.com', 'youtube-nocookie.com', $html);
 }
-add_filter( 'embed_oembed_html', 'ev_youtube_nocookie_oembed' ); // WordPress
-add_filter( 'video_embed_html', 'ev_youtube_nocookie_oembed' ); //Jetpack
+add_filter('embed_oembed_html', 'ev_youtube_nocookie_oembed'); // WordPress
+add_filter('video_embed_html', 'ev_youtube_nocookie_oembed'); //Jetpack
 
 
 
-function remove_api () {
-	remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
-	remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
+function remove_api()
+{
+    remove_action('wp_head', 'rest_output_link_wp_head', 10);
+    remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);
 }
-add_action( 'after_setup_theme', 'remove_api' );
+add_action('after_setup_theme', 'remove_api');
 
 
 
@@ -106,58 +143,60 @@ add_action( 'after_setup_theme', 'remove_api' );
 
 /**
  * Setup default Astra settings for a single post layout for specific post type.
- * 
- * This Removes header, Footer, Post title, Featured image, Post navigation. 
+ *
+ * This Removes header, Footer, Post title, Featured image, Post navigation.
  * Sets content layout to "Fullwidth Stretched" and sidear layout to `left-siebar`
  */
-function your_prefix_disale_header_footer() {
-	// bail early if the current post type if not the one we want to customize.
-	// 
-	if ( 'events' !== get_post_type() && is_singular() ) {
-		return;
-	}
-	// Disable Header, Above/below header, Sticky header.
-	//remove_action( 'astra_header', 'astra_header_markup' );
-	// Disable Footer widgets, Footer bar.
-	//remove_action( 'astra_footer', 'astra_footer_markup' );
-	// Set the page page layout to 'no-sidebar'.
-	add_filter( 'astra_page_layout', 'your_prefix_change_page_layout' );
-	// Set the content layout to 'page-builde' i.e. Fullwidth Stretched.
-	//add_filter( 'astra_get_content_layout', 'your_prefix_astra_content_layout' );
-	// Disable Post title.
-	//add_filter( 'astra_the_title_enabled', '__return_false' );
-	// Disale Featured image.
-	//add_filter( 'astra_featured_image_enabled', '__return_false' );
-	// Disale post navigation links.
-	add_filter( 'astra_single_post_navigation_enabled', '__return_false' );
-	// Disable Comments.
-	// This filter is from WordPress filter to disable comments.
-	add_filter( 'comments_open', '__return_false' );
+function your_prefix_disale_header_footer()
+{
+    // bail early if the current post type if not the one we want to customize.
+    //
+    if ('events' !== get_post_type() && is_singular()) {
+        return;
+    }
+    // Disable Header, Above/below header, Sticky header.
+    //remove_action( 'astra_header', 'astra_header_markup' );
+    // Disable Footer widgets, Footer bar.
+    //remove_action( 'astra_footer', 'astra_footer_markup' );
+    // Set the page page layout to 'no-sidebar'.
+    add_filter('astra_page_layout', 'your_prefix_change_page_layout');
+    // Set the content layout to 'page-builde' i.e. Fullwidth Stretched.
+    //add_filter( 'astra_get_content_layout', 'your_prefix_astra_content_layout' );
+    // Disable Post title.
+    //add_filter( 'astra_the_title_enabled', '__return_false' );
+    // Disale Featured image.
+    //add_filter( 'astra_featured_image_enabled', '__return_false' );
+    // Disale post navigation links.
+    add_filter('astra_single_post_navigation_enabled', '__return_false');
+    // Disable Comments.
+    // This filter is from WordPress filter to disable comments.
+    add_filter('comments_open', '__return_false');
 }
-add_action( 'wp', 'your_prefix_disale_header_footer' );
+add_action('wp', 'your_prefix_disale_header_footer');
 /**
  * Returns string for  no sidear layout.
- * 
+ *
  * @return 'String' String to set the page layout to 'no-sidebar'.
  */
-function your_prefix_change_page_layout () {
-	return 'right-sidebar';
+function your_prefix_change_page_layout()
+{
+    return 'right-sidebar';
 }
 /**
  * String to set Content layout to Fullwidth Stretched.
- * 
+ *
  * @return 'String' String 'page-builder' as the content layout.
  */
 //function your_prefix_astra_content_layout() {
-//	return 'page-builder';
+//  return 'page-builder';
 //}
 
-function tec_remove_sidebar(){
-	//get page type early if not search bail dude!
-	if ( !is_search() ) {
-		return;
-	}
-	add_filter( 'astra_page_layout', 'no-sidebar' );
-	
+function tec_remove_sidebar()
+{
+    //get page type early if not search bail dude!
+    if (!is_search()) {
+        return;
+    }
+    add_filter('astra_page_layout', 'no-sidebar');
 }
-add_action( 'wp', 'tec_remove_sidebar' );
+add_action('wp', 'tec_remove_sidebar');
